3d036ff039ec0933b62964919d2150112aa12f9c
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-03-25 00:53:26 +0100

    thin

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,13 @@
 #![feature(trace_macros)]
 #![feature(auto_traits)]
 #![feature(negative_impls)]
+#![feature(strict_provenance)]
 
 pub mod cfg_match;
 pub mod innocent_linked_list;
 pub mod scratch;
 pub mod sendsync;
+pub mod thin_u128;
 pub mod unroll_int;
 pub mod unsized_clone;
 
